c8b9b8cb4e74320957d6728394edafbcae61b405
Eric <ericzjchee@gmail.com>
2024-02-03 15:00:08 +0800

    finiede q1

--- main.py
+++ main.py
@@ -3,6 +3,30 @@
 Q1: 
 '''
 
+
+
+
+weight = float(input('Your weight: '))
+
+height = float(input('Your height: '))
+
+bmi = weight / (height * height)
+
+
+if bmi < 18.5:
+   print('Underweight')
+elif bmi <= 24.9 :
+   print('normal weight')
+elif bmi <= 25.0:
+   print('overweight')
+elif bmi > 65:
+   print('class 1 obesity')
+elif bmi > 65:
+   print('class 2 obesity')
+else:
+   print('class 3 obesity')
+
+
 # TODO: Write your code here
 
 ##################################################
